0dfeb4bab138f63fcc1421018e058157d42479a8
José Arlando Meireles Pinheiro Martins <arlandompm@gmail.com>
2026-05-29 15:40:10 -0400

    INTERFACE CRIADA (29/05/26)

---
+++ CÓDIGO/INTERFACE/Primeira.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+class Primeira {
+    public static void main (String[] args){
+        //CRIAR
+        JFrame primeira = new JFrame("Primeira Interface");
+        //CONFIGURAR
+        
+        primeira.setBounds(500, 220, 500, 500);
+        //TORNAR VISÍVEL
+        primeira.setVisible(true);
+    }
+}
